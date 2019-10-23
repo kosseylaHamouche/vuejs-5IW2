97febbe27ebfa40c564a44601272f4baad6870de
luffy <kosseyla.hamouche@gmail.com>
2019-10-23 16:30:38 +0200

    add modal

---
+++ client/src/components/NewCardPlusPlus.vue
@@ -0,0 +1,62 @@
+<template>
+    <Modal :open="open" :onClose="onClose" title="New Card">
+        <template slot="default">
+            <div class="grid-row">
+                <label for="title">Title</label>
+                <input v-model="name" id="title"/>
+            </div>
+            <div class="grid-row">
+                <label for="description">Description</label>
+                <textarea v-model="description" id="description"/>
+            </div>
+            <div class="grid-row">
+                <label for="velocity">Velocity</label>
+                <input v-model="velocity" id="velocity"/>
+            </div>
+            <div class="grid-row">
+                <label>Status</label>
+                <label>
+                    New
+                    <input type="radio" v-model="status" value="new" name="status" />
+                </label>
+                <label>
+                    Done
+                    <input type="radio" v-model="status" value="done" name="status" />
+                </label>
+            </div>
+        </template>
+        <template slot="footer">
+            <button @click="handleSubmit">Submit</button>
+        </template>
+    </Modal>
+</template>
+
+<script>
+export default {
+    name: "NewCard",
+    data: () => ({
+        name: "",
+        description: "",
+        velocity: 0,
+        status: "new"
+    }),
+    props: {
+        open: Boolean,
+        onClose: Function
+    },
+    methods: {
+        handleSubmit: function() {
+            this.$emit('new-card', this.$data);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.grid-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
